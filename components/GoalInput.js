2b42acee8df2af485a73a31c5e88efd8be8ec397
Collin Frink <ccdftwin2@gmail.com>
2020-01-25 23:28:35 -0600

    files

---
+++ components/GoalInput.js
@@ -0,0 +1,52 @@
+// import React, { useState } from "react";
+// import { View, TextInput, Button, StyleSheet, Modal } from "react-native";
+
+// const GoalInput = props => {
+//   const [enteredGoal, setEnteredGoal] = useState("");
+
+//   const goalInputHandler = enteredText => {
+//     setEnteredGoal(enteredText);
+//   };
+//   const addGoalHandler = () => {
+//     props.onAddGoal(enteredGoal);
+//     setEnteredGoal("");
+//   };
+//   const cancelGoalAdditionHandler = () => {
+//     props.onCancel();
+//     setEnteredGoal("");
+//   };
+//   return (
+//     <Modal visible={props.visible} animationType="slide">
+//       <View style={styles.inputContainer}>
+//         <TextInput
+//           placeholder="Course Goal"
+//           style={styles.input}
+//           onChangeText={goalInputHandler}
+//           value={enteredGoal}
+//         />
+//         <Button
+//           title="CANCEL"
+//           color="red"
+//           onPress={cancelGoalAdditionHandler}
+//         />
+//         <Button title="ADD" onPress={addGoalHandler} />
+//       </View>
+//     </Modal>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   inputContainer: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center"
+//   },
+//   input: {
+//     width: 300,
+//     borderColor: "black",
+//     borderWidth: 1,
+//     padding: 10,
+//     marginBottom: 10
+//   }
+// });
+// export default GoalInput;
